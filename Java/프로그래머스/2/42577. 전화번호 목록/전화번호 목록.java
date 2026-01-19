75982a5781d8c08f46bba3e24e03ebea78ea44e2
가은 정 <127952295+khmandarrin@users.noreply.github.com>
2026-01-19 21:33:53 +0900

    [level 2] Title: 전화번호 목록, Time: 305.64 ms, Memory: 250 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        HashSet<String> set = new HashSet<> ();
+        
+        for (String phone: phone_book){
+            set.add(phone);
+        }
+        
+        for (String phone: phone_book){
+            for (int i = 1; i < phone.length(); i++){
+                String prefix = phone.substring(0, i);
+                
+                if (set.contains(prefix)){
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
+}
