2aa63b135a58bd5a4e2644f71f7d3736a137b45e
가은 정 <127952295+khmandarrin@users.noreply.github.com>
2026-04-05 23:00:15 +0900

    [level 0] Title: 가위 바위 보, Time: 0.05 ms, Memory: 79.8 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String rsp) {
+        StringBuilder answer = new StringBuilder();
+        for (char ch : rsp.toCharArray()){
+            if (ch == '2') answer.append('0');
+            else if (ch == '0') answer.append('5');
+            else answer.append('2');
+        }
+        return answer.toString();
+    }
+}
