7abbbed990ffabfb533942d5f74888cd79d25d06
가은 정 <127952295+khmandarrin@users.noreply.github.com>
2026-04-16 12:18:28 +0900

    [level 0] Title: 숫자 찾기, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = 0;
+        String str = String.valueOf(num);
+        for (int i = 0; i < str.length(); i++){
+            if ((str.charAt(i) - '0') == k){
+            return i+1;
+            }
+        }
+        
+        return -1;
+    }
+}
